a1dd84eba13df6b902b9123ddbd079ef9364092a
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2018-11-20 16:11:06 +0100

    fix

--- src/Galcorp.Demo.Console/Program.cs
+++ src/Galcorp.Demo.Console/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using Galcorp.Auth.Google;
 
 namespace Galcorp.Demo.Console
 {
     class Program
     {
+        // client configuration
+        const string clientID = "581786658708-elflankerquo1a6vsckabbhn25hclla0.apps.googleusercontent.com";
+        const string clientSecret = "3f6NggMbPtrmIBpgx-MK2xXK";
+        const string authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+        const string tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
+        const string userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            var c = new GoogleClient();
+            var redirectUri = string.Format("http://{0}:{1}/", IPAddress.Loopback, GetRandomUnusedPort());
+            c.doOAuth(redirectUri, authorizationEndpoint, clientID, clientSecret);
+            
+        }
+
+        public static int GetRandomUnusedPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+
+        // Hack to bring the Console window to front.
+        // ref: http://stackoverflow.com/a/12066376
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        public void BringConsoleToFront()
+        {
+            SetForegroundWindow(GetConsoleWindow());
         }
     }
 }
